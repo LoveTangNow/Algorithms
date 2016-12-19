b1afa9799666cb22a7f8cdaec6b47be5e74243f5
Smart-Ha <865469028@qq.com>
2016-12-19 22:37:50 +0800

    add numberOf1Between1AndN

--- src/wha/basic/ArrayApp.java
+++ src/wha/basic/ArrayApp.java
@@ -233,7 +233,12 @@ public class ArrayApp {
         return count;
     }
 
-
+    public int numberOf1Between1AndNII(int n) {
+        int ones = 0;
+        for (long m = 1; m <= n; m *= 10)
+            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1 ? n%m + 1 : 0);
+        return ones;
+    }
 
 
 }
