4733f0a5b1c1a148b850ec56cbb3f14d12779dd9
Smart-Ha <865469028@qq.com>
2016-12-27 22:08:54 +0800

    add add-function.

--- src/wha/basic/MathApp.java
+++ src/wha/basic/MathApp.java
@@ -262,9 +262,37 @@ public class MathApp {
         lastRemaining(11,7);
     }
 
+    /**
+     * 求1+2+3+...+n的值,不用循环和判断语句
+     */
     public int Sum_Solution(int n) {
         int sum = n;
         boolean b = (n >0) && ((sum += Sum_Solution(n-1))>0);
         return sum;
+    }
+
+    /**
+     *写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号
+     *  5-101，7-111
+     *  第一步：相加各位的值，不算进位，得到010，二进制每位相加就相当于各位做异或操作，101^111。
+     第二步：计算进位值，得到1010，相当于各位做与操作得到101，再向左移一位得到1010，(101&111)<<1。
+     第三步重复上述两步， 各位相加 010^1010=1000，进位值为100=(010&1010)<<1。
+     继续重复上述两步：1000^100 = 1100，进位值为0，跳出循环，1100为最终结果。
+     */
+
+    public int add(int num1,int num2) {
+        int temp;
+        while(num2 != 0){
+            temp = num1 ^ num2;
+            num2 = (num1 & num2)<<1;
+            num1 = temp;
+        }
+        return num1;
+    }
+
+    @Test
+    public void addTest(){
+        int a=1,b=2;
+        System.out.println(add(a,b));
     }
 }
