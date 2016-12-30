5112886fefc8f41188b7cf0fb2a101cb9266d096
Smart-Ha <865469028@qq.com>
2016-12-30 22:31:02 +0800

    add function: isSymmetrical

--- src/wha/tree/BinaryTree.java
+++ src/wha/tree/BinaryTree.java
@@ -250,4 +250,51 @@ public class BinaryTree {
         }
         return null;
     }
+
+
+    /**
+     * 请实现一个函数，用来判断一颗二叉树是不是对称的。
+     * 注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+     */
+    public boolean isSymmetrical(TreeNode pRoot)  {
+        if(pRoot == null) return true;
+
+        TreeNode left = pRoot.left,right = pRoot.right;
+        if(left == null && right == null){
+            return true;
+        }
+
+        if(isSymmetricalProc(left,right)){
+            return true;
+        }
+        return false;
+
+    }
+
+    private boolean isSymmetricalProc(TreeNode left, TreeNode right) {
+        if(left == null && right == null)   return true;
+        if(left != null && right != null){
+            if(left.val == right.val && isSameTree(left.left,right.right) &&
+                    isSameTree(left.right,right.left)){
+                return true;
+            }else return false;
+        }
+
+        return false;
+    }
+
+    /**
+     * 交换左右子树
+     * @param root
+     */
+    private void exchangeTreeNode(TreeNode root) {
+        if(root == null)    return;
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        exchangeTreeNode(root.left);
+        exchangeTreeNode(root.right);
+    }
+
+
 }
